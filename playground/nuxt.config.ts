b2f5b5ffe8c4582bf3cb34104b1891279dbbf708
unknown <nexoscreator@gmail.com>
2025-02-28 11:57:54 +0530

    chore: update dependencies

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -17,7 +17,16 @@ export default defineNuxtConfig({
   googleTranslate: {
     defaultLanguage: 'en',
     supportedLanguages: [
-      'te', 'tr', 'uk', 'ur', 'vi', 'cy', 'yo', 'zu', 'en',
+      'ab', 'ace', 'ach', 'af', 'sq', 'alz', 'am', 'ar', 'hy', 'as', 'awa', 'ay', 'az',
+      'ban', 'bm', 'ba', 'eu', 'btx', 'bts', 'bbc', 'be', 'bem', 'bn', 'bew', 'bho',
+      'bik', 'bs', 'br', 'bg', 'bua', 'yue', 'ca', 'ceb', 'ny', 'zh-CN', 'zh-TW', 'cv',
+      'co', 'crh', 'hr', 'cs', 'da', 'din', 'dv', 'doi', 'dov', 'nl', 'dz', 'en', 'eo',
+      'et', 'ee', 'fj', 'fil', 'fi', 'fr', 'fr-FR', 'fr-CA', 'fy', 'ff', 'gaa', 'gl',
+      'lg', 'ka', 'de', 'el', 'gn', 'gu', 'ht', 'cnh', 'ha', 'haw', 'he', 'hil', 'hi',
+      'hmn', 'hu', 'is', 'ig', 'ilo', 'id', 'ga', 'it', 'ja', 'jv', 'kn', 'pam', 'kk',
+      'km', 'rw', 'ko', 'ku', 'ckb', 'lv', 'lt', 'mk', 'ms', 'ml', 'mt', 'mr', 'mn',
+      'ne', 'no', 'fa', 'pl', 'pt', 'pa', 'ro', 'ru', 'sr', 'es', 'sw', 'sv', 'ta',
+      'te', 'tr', 'uk', 'ur', 'vi', 'cy', 'yo', 'zu',
     ],
   },
 })
